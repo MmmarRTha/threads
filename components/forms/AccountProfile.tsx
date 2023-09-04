745585e68cf7a3c40bf36f1ad751fd450b3988f2
MmmarRTHa <m27842n@outlook.com>
2023-09-03 22:14:48 -0700

    - update: main - added connection to mongodb, saved update profile user.

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -4,7 +4,6 @@ import { Button } from "@/components/ui/button";
 import {
     Form,
     FormControl,
-    FormDescription,
     FormField,
     FormItem,
     FormLabel,
@@ -20,6 +19,9 @@ import {ChangeEvent, useState} from "react";
 import {Textarea} from "@/components/ui/textarea";
 import {isBase64Image} from "@/lib/utils";
 import {useUploadThing} from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
+
 
 interface Props {
     user: {
@@ -35,6 +37,8 @@ interface Props {
 const AccountProfile = ({user, btnTitle}: Props) => {
     const [files, setFiles] = useState<File[]>([]);
     const { startUpload } = useUploadThing("media");
+    const router = useRouter();
+    const pathname = usePathname();
 
     const form = useForm({
         resolver: zodResolver(UserValidation) ,
@@ -69,14 +73,26 @@ const AccountProfile = ({user, btnTitle}: Props) => {
     const onSubmit = async (values: z.infer<typeof UserValidation>) => {
         const blob = values.profile_photo;
         const hasImageChanged = isBase64Image(blob);
-
         if(hasImageChanged) {
             const imgRes = await startUpload(files);
-            if(imgRes) {
+            if(imgRes && imgRes[0].url) {
                 values.profile_photo = imgRes[0].url;
             }
         }
-        // TODO: Update user profile
+        await updateUser({
+            userId: user.id,
+            username: values.username,
+            name: values.name,
+            bio: values.bio,
+            image: values.profile_photo,
+            path: pathname
+        });
+
+        if(pathname === "/profile/edit") {
+            router.back();
+        } else {
+            router.push("/");
+        }
     }
 
     return (
